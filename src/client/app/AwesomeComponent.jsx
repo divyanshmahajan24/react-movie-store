177cf9adf3721424e544d2288d1e8c268dc7c956
Divyansh Mahajan <divyansh.mahajan@innovaccer.com>
2017-10-16 00:08:34 +0530

    first movie app draft

--- src/client/app/AwesomeComponent.jsx
+++ src/client/app/AwesomeComponent.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
+const axios = require('axios');
 
 class AwesomeComponent extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {likesCount : 0};
+    console.log(this.props);
+    this.state = {likesCount : 0, movies: []};
+    this.onMovies = this.onMovies.bind(this);
     this.onLike = this.onLike.bind(this);
   }
+
 
   onLike () {
     let newLikesCount = this.state.likesCount + 1;
     this.setState({likesCount: newLikesCount});
+  }
+
+  onMovies () {
+    let newMovies;
+    axios.get('http://www.theimdbapi.org/api/find/movie?title={love}')
+    .then(function (response) {
+      newMovies = response.data;
+      this.setState({movies: newMovies});
+    }.bind(this))
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
   render() {
@@ -18,6 +34,15 @@ class AwesomeComponent extends React.Component {
       <div>
         Likes : <span>{this.state.likesCount}</span>
         <div><button onClick={this.onLike}>Like Me</button></div>
+        <div>Movies:</div>
+        <div>
+          <table>
+            <tbody>
+              {this.state.movies.map(movie => {return <tr key={movie.imdb_id}><td>{movie.title}</td></tr>})}
+            </tbody>
+          </table>
+        </div>
+        <div><button onClick={this.onMovies}>Get Movies</button></div>
       </div>
     );
   }
